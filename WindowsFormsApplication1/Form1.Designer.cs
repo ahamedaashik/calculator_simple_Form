496b806a7d42f33280ccbee27c26aee1b8b4d3dc
ahamedaashik <34390159+ahamedaashik@users.noreply.github.com>
2022-10-13 10:59:18 +0530

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.num0 = new System.Windows.Forms.Button();
+            this.Float = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mult = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.rad_0 = new System.Windows.Forms.RadioButton();
+            this.rad_1 = new System.Windows.Forms.RadioButton();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(12, 111);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(43, 23);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(72, 111);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(43, 23);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(12, 158);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(43, 23);
+            this.num4.TabIndex = 3;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(72, 158);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(43, 23);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(12, 204);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(43, 23);
+            this.num7.TabIndex = 6;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(72, 204);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(43, 23);
+            this.num8.TabIndex = 7;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(138, 204);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(43, 23);
+            this.num9.TabIndex = 8;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(138, 111);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(43, 23);
+            this.num3.TabIndex = 2;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(138, 158);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(43, 23);
+            this.num6.TabIndex = 5;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(72, 251);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(43, 23);
+            this.num0.TabIndex = 10;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // Float
+            // 
+            this.Float.Location = new System.Drawing.Point(138, 251);
+            this.Float.Name = "Float";
+            this.Float.Size = new System.Drawing.Size(43, 23);
+            this.Float.TabIndex = 11;
+            this.Float.Text = ".";
+            this.Float.UseVisualStyleBackColor = true;
+            this.Float.Click += new System.EventHandler(this.Float_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(285, 111);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(43, 23);
+            this.add.TabIndex = 12;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(285, 158);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(43, 23);
+            this.sub.TabIndex = 13;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(285, 187);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(43, 23);
+            this.div.TabIndex = 14;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            // 
+            // mult
+            // 
+            this.mult.Location = new System.Drawing.Point(285, 226);
+            this.mult.Name = "mult";
+            this.mult.Size = new System.Drawing.Size(43, 23);
+            this.mult.TabIndex = 15;
+            this.mult.Text = "*";
+            this.mult.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(47, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // rad_0
+            // 
+            this.rad_0.AutoSize = true;
+            this.rad_0.Location = new System.Drawing.Point(244, 22);
+            this.rad_0.Name = "rad_0";
+            this.rad_0.Size = new System.Drawing.Size(14, 13);
+            this.rad_0.TabIndex = 20;
+            this.rad_0.TabStop = true;
+            this.rad_0.UseVisualStyleBackColor = true;
+            // 
+            // rad_1
+            // 
+            this.rad_1.AutoSize = true;
+            this.rad_1.Location = new System.Drawing.Point(244, 51);
+            this.rad_1.Name = "rad_1";
+            this.rad_1.Size = new System.Drawing.Size(14, 13);
+            this.rad_1.TabIndex = 21;
+            this.rad_1.TabStop = true;
+            this.rad_1.UseVisualStyleBackColor = true;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(285, 295);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(61, 23);
+            this.exit.TabIndex = 22;
+            this.exit.Text = "exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 339);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.rad_1);
+            this.Controls.Add(this.rad_0);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.mult);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.Float);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Calculator with NumPad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button Float;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mult;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton rad_0;
+        private System.Windows.Forms.RadioButton rad_1;
+        private System.Windows.Forms.Button exit;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //var input1;
+            InitializeComponent();
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+             textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+                textBox1.Text = textBox1.Text + (int.Parse(num2.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num2.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+                textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+                textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+                textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString(); 
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+                textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+                textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+             textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+             textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            if (rad_0.Checked)
+            {
+             textBox1.Text = textBox1.Text + (int.Parse(num1.Text)).ToString();
+            }
+            else if (rad_1.Checked)
+                textBox2.Text = textBox2.Text + (int.Parse(num1.Text)).ToString();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            var input_0 = int.Parse(textBox1.Text);
+            var input_1 = int.Parse(textBox2.Text);
+            textBox3.Text = (input_0 + input_1).ToString();
+        }
+
+        private void Float_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = textBox1.Text + (Float.Text).ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
